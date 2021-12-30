fa5c4a103524bd5f5abc9e9f85e7ced749954549
solaie12 <s.seshiferaw@gmail.com>
2021-12-30 09:14:13 +0100

    adding test cases

--- Recurrsion/change_in_foreign_currency.py
+++ Recurrsion/change_in_foreign_currency.py
@@ -30,21 +30,22 @@ def change_in_currency(denom, target):
 
 # print(change_in_currency([16,17,29],75))
 
-print(change_in_currency([5,10,25,100,200],94)== False)
+# print(change_in_currency([5,10,25,100,200],94)== False)
 
 
-print(change_in_currency([6,10,25,100,200],96)== True)
+# print(change_in_currency([6,10,25,100,200],96)== True)
 
 
-print(change_in_currency([10,25,100,200],96) == False)
+# print(change_in_currency([10,25,100,200],96) == False)
 
-print(change_in_currency([10,15,17],97) == True)
-print(change_in_currency([4, 17, 29],75) == True)
-print(change_in_currency([1, 5, 10],7) == True)
-print(change_in_currency([5, 10, 25, 100, 200],94) == False)
-print(change_in_currency([200],20) == False)
-print(change_in_currency([3, 6, 9, 12, 15],1000000) == False)
-print(change_in_currency([54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10,
-                          8, 6, 4, 2], 999999) == False)
+# print(change_in_currency([10,15,17],97) == True)
+# print(change_in_currency([4, 17, 29],75) == True)
+# print(change_in_currency([1, 5, 10],7) == True)
+# print(change_in_currency([5, 10, 25, 100, 200],94) == False)
+# print(change_in_currency([200],20) == False)
+# print(change_in_currency([3, 6, 9, 12, 15],1000000) == False)
+# print(change_in_currency([54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10,
+#                           8, 6, 4, 2], 999999) == False)
 
 
+ 
